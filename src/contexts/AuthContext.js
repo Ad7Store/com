537f01360fa24812186e3acc88da75cdcc8bc34b
Ad7Store <ad7store.log@gmail.com>
2025-12-29 16:39:29 +0500

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,7 @@
+'use client'
+
 import { createContext, useState, useContext, useEffect, useCallback } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import Cookies from 'js-cookie'
 import axios from 'axios'
 import { toast } from 'react-hot-toast'
